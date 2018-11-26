a361032db903016bba9c4c1dc812619e46cb1899
Travis T <travistokuyama@gmail.com>
2018-11-26 21:49:42 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,12 @@ class UsersController < ApplicationController
     erb :'users/new'
   end
 
+  post '/signup' do
+    @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
+    @user.save
+    session[:user_id] = @user.id
+    redirect to '/tweets'
+  end
+
 
 end
